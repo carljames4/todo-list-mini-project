118bc4635ed1e876817f7ff9616df513527bd536
carljames4 <magsipocjames93@gmail.com>
2026-06-28 12:14:19 +0800

    Initial commit

---
+++ todolist/script.js
@@ -0,0 +1,24 @@
+        let list = [];
+
+        function addlist() {
+            let value = document.getElementById("input").value;
+
+            list.push(value);
+
+            showlist();
+        }
+
+        function showlist() {
+            let text = ""
+            for(let i = 0; i < list.length ; i++) {
+                text += `${list[i]} <button onclick="deletelist(${i})">delete</button><br>`;
+
+            }
+                document.getElementById("output").innerHTML = text;
+        }
+
+        function deletelist(index) {
+            list.splice(index, 1);
+            showlist();
+
+        }
